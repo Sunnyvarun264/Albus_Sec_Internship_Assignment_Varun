61aed104e1dd77c779512ea4fa76f58dfcc17114
Sunnyvarun264 <85699599+Sunnyvarun264@users.noreply.github.com>
2024-05-17 15:36:32 +0530

    Create Q.2 Hours to Seconds Converter.py

---
+++ python assignment/Assignment (2)/Q.2 Hours to Seconds Converter.py
@@ -0,0 +1,9 @@
+# Q.2 Write a function that converts hours into seconds.
+
+def hours_to_seconds(hours):
+    return hours * 3600
+
+# Example usage
+hours = 2
+seconds = hours_to_seconds(hours)
+print(f"{hours} hours is equal to {seconds} seconds.")
