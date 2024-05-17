5b22de22453f8b012a574996fc24875fb24da729
Sunnyvarun264 <85699599+Sunnyvarun264@users.noreply.github.com>
2024-05-17 15:09:12 +0530

    Create Conditionals.py

---
+++ Python Assignment/Assignment-1/Conditionals.py
@@ -0,0 +1,22 @@
+# Conditional Statements
+
+# i) if statement
+num = 10
+if num > 0:
+    print(f"{num} is a positive number.")
+
+# ii) if else statement
+num = -5
+if num > 0:
+    print(f"{num} is a positive number.")
+else:
+    print(f"{num} is a negative number.")
+
+# iii) if – elif – else statement
+num = 0
+if num > 0:
+    print(f"{num} is a positive number.")
+elif num == 0:
+    print(f"{num} is zero.")
+else:
+    print(f"{num} is a negative number.")
